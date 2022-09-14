c1ceabdeb61b20676b64df4ad6ae7d6f35cfa292
munshodak0518 <113504602+munshodak0518@users.noreply.github.com>
2022-09-14 12:37:22 +0500

    Add files via upload

---
+++ Amaliy ishlarim/app.py
@@ -0,0 +1,75 @@
+# Bu bir qatorli komment
+
+"""Bir nechta qatorli
+kommentariya
+yozish kerak"""
+# a=25
+# b=45
+# print(a+b)
+# print(5+12)
+
+# c=1+5j
+# print(a,b)
+# a=35
+# print(a)
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# txt='Salom'
+# txt2="Salom"
+# txt3="""
+#     Mening ismim Alijon
+#     yoshim 12da
+#     namangan shahri
+# """
+# print(txt)
+# print(txt2)
+# print(txt3,type(txt3))
+# isGrand=False
+# print(not isGrand,type(isGrand))
+# a=int(input('a sonini kiriting: '))
+# b=int(input('b sonini kiriting: '))
+# c=int(input('c sonini kiriting: '))
+# d=(a+b+c)/3
+#
+# intD=int(d)
+# strD=str(d)
+#
+# print(type(intD))
+# print(type(strD))
+# ism='Alijon'
+# yosh=25
+# print(ism+' '+str(yosh))
+# print(ism,yosh)
+# a=int(input('a soni: '))
+# b=int(input('b soni: '))
+# a=a+b #11
+# b=a-b #11-6=5
+# a=a-b #11-5=6
+# print(a,b)
+# print(2*2)
+# print(2**3) #2*2*2
+# a=8
+# b=5
+# print(a//b)
+# print(int(a/b))
+# print(a%b)
+
+# import math as m
+# from math import sqrt,fabs,ceil,floor
+# from math import *
+# a=25.25
+# print(sqrt(a))
+# print(fabs(a))
+# print(ceil(a))
+# print(floor(a))
+# a=5
+# b=8
+# c=3
+# print(a<b<c)
+
+# print(m.sqrt(a))
+# print(m.fabs(a))
+# print(m.ceil(a))
+# print(m.floor(a))
